8b82f6199e73368d4cc9810d4fcc81995efb51cd
Hope Gong <153147497+hopeg0212@users.noreply.github.com>
2024-12-07 06:08:45 -0500

    Update stg_tb_property_tax.sql

--- dbt-cloud/models/staging/stg_tb_property_tax.sql
+++ dbt-cloud/models/staging/stg_tb_property_tax.sql
@@ -7,15 +7,15 @@ WITH base AS (
         county,
         school_code,
         school_name,
-        type_of_value_on_whichtax_rates_are_applied,
+        type_of_value_on_which_tax_rates_are_applied,
         county_tax_levy,
-        county_tax_rate_outside_village_per_1000_assessed_value,
-        county_tax_rate_inside_village_per_1000_assessed_value,
+        county_tax_rate_outside_village_per_1000_value,
+        county_tax_rate_inside_village_per_1000_value,
         municipality_tax_levy,
-        municipal_tax_rate_outside_village_per_1000_assessed_value,
-        municipal_tax_rate_inside_village_per_1000_assessed_value,
+        municipal_tax_rate_outside_village_per_1000_value,
+        municipal_tax_rate_inside_village_per_1000_value,
         school_district_tax_levy,
-        school_district_tax_rate_per_1000_assessed_value
+        school_district_tax_rate_per_1000_value
     FROM `cis4400-hw-443410.cleaned_data.property_tax_rates`
 )
 SELECT * FROM base;
