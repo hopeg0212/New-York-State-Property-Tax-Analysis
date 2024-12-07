3378cee871b1eba8c02292b43b5adabdf886ec99
Hope Gong <153147497+hopeg0212@users.noreply.github.com>
2024-12-07 06:04:35 -0500

    Update Highest Taxed School Districts.sql

--- dbt-cloud/models/analysis/Highest Taxed School Districts.sql
+++ dbt-cloud/models/analysis/Highest Taxed School Districts.sql
@@ -1,6 +1,6 @@
 SELECT 
     school_name,
-    MAX(school_district_tax_rate_per_1000_assessed_value) AS max_school_tax_rate
+    MAX(school_district_tax_rate_per_1000_value) AS max_school_tax_rate
 FROM 
     `cis4400-hw-443410.cleaned_data.property_tax_rates`
 GROUP BY 
