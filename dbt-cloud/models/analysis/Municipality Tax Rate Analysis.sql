1e85660ed87e9e702e6e0d679758fe7e4d4496a6
Hope Gong <153147497+hopeg0212@users.noreply.github.com>
2024-12-07 06:05:05 -0500

    Update Municipality Tax Rate Analysis.sql

--- dbt-cloud/models/analysis/Municipality Tax Rate Analysis.sql
+++ dbt-cloud/models/analysis/Municipality Tax Rate Analysis.sql
@@ -1,6 +1,6 @@
 SELECT 
     municipality,
-    AVG(municipal_tax_rate_outside_village_per_1000_assessed_value) AS avg_municipal_tax_rate_outside_village
+    AVG(municipal_tax_rate_outside_village_per_1000_value) AS avg_municipal_tax_rate_outside_village
 FROM 
     `cis4400-hw-443410.cleaned_data.property_tax_rates`
 GROUP BY 
