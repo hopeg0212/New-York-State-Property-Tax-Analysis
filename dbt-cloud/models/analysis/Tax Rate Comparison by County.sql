c1edce9c84e61b36befc282d9eccd3bed5b05f99
Hope Gong <153147497+hopeg0212@users.noreply.github.com>
2024-12-07 06:05:56 -0500

    Update Tax Rate Comparison by County.sql

--- dbt-cloud/models/analysis/Tax Rate Comparison by County.sql
+++ dbt-cloud/models/analysis/Tax Rate Comparison by County.sql
@@ -1,7 +1,7 @@
 SELECT 
     county,
-    AVG(county_tax_rate_outside_village_per_1000_assessed_value) AS avg_tax_rate_outside_village,
-    AVG(county_tax_rate_inside_village_per_1000_assessed_value) AS avg_tax_rate_inside_village
+    AVG(county_tax_rate_outside_village_per_1000_value) AS avg_tax_rate_outside_village,
+    AVG(county_tax_rate_inside_village_per_1000_value) AS avg_tax_rate_inside_village
 FROM 
     `cis4400-hw-443410.cleaned_data.property_tax_rates`
 GROUP BY 
